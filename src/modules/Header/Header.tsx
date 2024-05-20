e4d44669b50b964e643cfd4e828ecd38e55ce101
IrynaDidkivska <96765824+IrynaDidkivska@users.noreply.github.com>
2024-05-20 22:07:47 +0200

    refactor: Update Header component styles for mobile menu

--- src/modules/Header/Header.tsx
+++ src/modules/Header/Header.tsx
@@ -21,19 +21,19 @@ const Header = () => {
         xl:px-36 xl:pt-8 xl:pb-4 
        `}
       >
-        <div className="flex items-center md:justify-between max-w-[1440px] m-auto xl:mb-12">
+        <div className="flex items-center sm:justify-start md:justify-between max-w-[1440px] m-auto xl:mb-12">
           <button
             onClick={() => setIsOpen(true)}
             type="button"
             aria-label="burger"
-            className="w-10 h-10 sm:mr-[71px] md:w-16 md:h-16 xl:hidden"
+            className="w-10 h-10  md:w-16 md:h-16 xl:hidden"
           >
             <SpriteSVG name="burger_menu" />
           </button>
           <div className="hidden xl:flex flex-col gap-[8px]  font-ttChocolates500 text-[16px] font-medium leading-6 tracking-wider">
             <Contacts />
           </div>
-          <Logo className="w-[66px] h-[54px] md:w-[126px] md:h-[102px]" />
+          <Logo className="m-auto w-[66px] h-[54px] md:w-[126px] md:h-[102px]" />
           <div className="hidden md:flex md:flex-col md:gap-2 md:max-w-[139px] xl:gap-4 xl:max-w-[168px] ">
             <Promotion />
             <SocialMedia />
@@ -45,7 +45,7 @@ const Header = () => {
         <Modal
           isOpen={isOpen}
           onClose={toggleModal}
-          contentClassName="top-0 left-0 transform -translate-x-0 -translate-y-0 sm:px-6 sm:pb-4 sm:pt-[78px] sm:max-w-[320px]
+          contentClassName="top-0 left-0 transform -translate-x-0 -translate-y-0 sm:px-6 sm:pb-4 sm:pt-[78px] sm:max-w-[320px] h-lvh
           md:p-0  md:max-w-[336px] rounded-none"
         >
           <MobileMenu
